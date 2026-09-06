e0c3ae5b325aa3ad44c7e708102f4a507b78b9a8
raulvilera <1.24225576e+08+raulvilera@users.noreply.github.com>
2026-09-06 23:40:45 +0000

    Conecta atividade a implantacao final com roscas

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const className = document.getElementById('className');
 const studentName = document.getElementById('studentName');
 const statusMessage = document.getElementById('statusMessage');
 const storageKey = 'atividade-meteorologia-8ano-respostas';
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxsG6l2r7udl9DqvjhJbKSipTisYoZ3hPB5ajlcLj8iE6mjfa_VTeQs1eQkNUfZkvAs/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzWmgnQ5Yk9LiZzSRiMmRwf393qMMQKyY3eHceEwfzK2hn7Jt-iGbLDoVPZWGhsYD_I/exec';
 const metadataFields = ['callNumber', 'institutionalEmail', 'raNumber', 'raDigit', 'activityDate'];
 
 function todayAsInputDate() {
